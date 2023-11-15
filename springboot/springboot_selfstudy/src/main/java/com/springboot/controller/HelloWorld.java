90687909e4b6f42d03da57d3f7b4ac53275883f1
yibai <404223879@qq.com>
2023-11-15 12:38:30 +0800

    value Environment获取yaml文件中的配置信息

--- springboot/springboot_selfstudy/src/main/java/com/springboot/controller/HelloWorld.java
+++ springboot/springboot_selfstudy/src/main/java/com/springboot/controller/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,14 @@ public class HelloWorld {
     @RequestMapping ("/hello")
     public String hello() {
         return "hello springboot and i love you muqian";
+    }
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/s")
+    public void s() {
+        System.out.println(env.getProperty("server.port"));
+        System.out.println(env.getProperty("address[0]"));
     }
 }
